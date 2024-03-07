c18050354c94888e4c47e6040ccbed82ee74a4be
Wai Lin Oo <41633762+wailinoo1@users.noreply.github.com>
2024-03-07 13:57:29 +0630

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 896836667748.dkr.ecr.ap-southeast-1.amazonaws.com
-docker stop nodejs || true
-docker rm nodejs || true
-echo "Pulling and running the new Docker container..."
-docker run -d -p 8080:8080 896836667748.dkr.ecr.ap-southeast-1.amazonaws.com/wlo-private-repo:latest
